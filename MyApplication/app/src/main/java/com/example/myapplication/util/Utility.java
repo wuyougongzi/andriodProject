086b7777a836439400c125df664369650c379ea6
wuyougongzi <1211385701@qq.com>
2016-03-12 23:32:08 +0800

    1 add Utility functions

--- MyApplication/app/src/main/java/com/example/myapplication/util/Utility.java
+++ MyApplication/app/src/main/java/com/example/myapplication/util/Utility.java
@@ -1,14 +1,18 @@
 package com.example.myapplication.util;
 
+import java.lang.String;
 import android.text.TextUtils;
 
 import com.example.myapplication.db.CoolWeatherDB;
+import com.example.myapplication.model.City;
+import com.example.myapplication.model.Country;
 import com.example.myapplication.model.Province;
 
 /**
  * Created by chongge on 2016/3/12.
  */
 public class Utility {
+
     public synchronized static boolean handleProvinceResponce(CoolWeatherDB coolWeatherDB,
                                                               String response){
         if(!TextUtils.isEmpty(response)){
@@ -21,6 +25,47 @@ public class Utility {
                     province.setProvinceName(array[1]);
 
                     coolWeatherDB.saveProvince(province);
+                }
+                return  true;
+            }
+        }
+        return  false;
+    }
+
+    public synchronized static boolean handleCityResponse(CoolWeatherDB coolWeatherDB,
+                                                          String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            String[] allCity = response.split(",");
+            if(allCity != null && allCity.length > 0){
+                for(String p : allCity){
+                    String[] array = p.split("\\|");
+                    City city = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+
+                    coolWeatherDB.saveCity(city);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public synchronized static boolean handleCountryResponse(CoolWeatherDB coolWeatherDB,
+                                                             String response, int cityId){
+        if(!TextUtils.isEmpty(response)){
+            String[] allCountry = response.split(",");
+            if(allCountry != null && allCountry.length > 0){
+                for(String p : allCountry){
+                    String[] array =p.split("\\|");
+                    Country country = new Country();
+                    country.setCountryCode(array[0]);
+                    country.setCountryName(array[1]);
+                    country.setCityId(cityId);
+
+                    coolWeatherDB.saveCountry(country);
                 }
                 return  true;
             }
