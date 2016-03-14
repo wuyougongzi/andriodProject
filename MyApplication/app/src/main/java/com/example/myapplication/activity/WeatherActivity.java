f99580a7144f211d152e3dc0c93ca2f704efff33
wuyougongzi <1211385701@qq.com>
2016-03-14 23:34:45 +0800

    1 add WeatherActivity to show current weather situation

---
+++ MyApplication/app/src/main/java/com/example/myapplication/activity/WeatherActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.chongge.myapplication.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
